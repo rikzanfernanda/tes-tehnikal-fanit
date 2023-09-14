17e3894599ba0a5710ce12b4fedba5896d82a9cf
rikzan fernanda <fernandarikzan@gmail.com>
2023-09-14 11:12:06 +0700

    count words

---
+++ tes-logika/src/countWords.js
@@ -0,0 +1,20 @@
+function countWords(sentence) {
+    const specialChars = ['*', '_', '!', '=', '[', ']', '(', ')', '{', '}', '&'];
+
+    const words = sentence.split(' ');
+
+    let count = 0;
+
+    for (const word of words) {
+        for (const char of word) {
+            if (specialChars.includes(char)) {
+                count++;
+                break;
+            }
+        }
+    }
+
+    return words.length - count;
+}
+
+export default countWords
---
+++ tes-logika/tests/countWords.test.js
@@ -0,0 +1,15 @@
+import countWords from "../src/countWords"
+
+describe('Hitung jumlah kata pada sebuah kalimat', () => {
+    test('should return 5', () => {
+        expect(countWords('Saat meng*ecat tembok, Agung dib_antu oleh Raihan.')).toBe(5)
+    })
+
+    test('should return 3', () => {
+        expect(countWords('Berapa u(mur minimal[ untuk !mengurus ktp?')).toBe(3)
+    })
+
+    test('should return 4', () => {
+        expect(countWords('Masing-masing anak mendap(atkan uang jajan ya=ng be&rbeda.')).toBe(4)
+    })
+})

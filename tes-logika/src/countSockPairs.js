3fe3f461249d480c4e413e2c8d26da7a047af151
rikzan fernanda <fernandarikzan@gmail.com>
2023-09-14 10:46:52 +0700

    count sock pairs

---
+++ tes-logika/src/countSockPairs.js
@@ -0,0 +1,20 @@
+const countSockPairs = (socks) => {
+    const sockCounts = {};
+    let pairCount = 0;
+
+    for (const sock of socks) {
+        if (sockCounts[sock]) {
+            sockCounts[sock]++;
+        } else {
+            sockCounts[sock] = 1;
+        }
+    }
+
+    for (const count of Object.values(sockCounts)) {
+        pairCount += Math.floor(count / 2);
+    }
+
+    return pairCount;
+}
+
+export default countSockPairs
